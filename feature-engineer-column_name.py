7ebadd62794aed2632f4fc4106ac401c2cc78dce
RamyaRagu2506 <102846337+RamyaRagu2506@users.noreply.github.com>
2022-08-25 17:50:04 +0200

    Add files via Upload
    First version of feature engineering for a small financial data column.

---
+++ feature-engineer-column_name.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+
+# Read the excel file given
+purchase_sales_listing = pd.read_excel("Purchase_sales_listing.xlsx",sheet_name="Sales")
+
+columnname_split = purchase_sales_listing.columns[0].split("-")
+values = purchase_sales_listing.values
+
+customernames = list()
+vechiclenumber =list()
+
+for i in values:
+    print(i[0].split("-"))
+    customernames.append(i[0].split("-")[0])
+    vechiclenumber.append(i[0].split("-")[1])
+
+purchase_sales_listing[columnname_split[0]] = customernames
+purchase_sales_listing[columnname_split[1]] = vechiclenumber
+
+new_sales_data = purchase_sales_listing.drop(columns=[purchase_sales_listing.columns[0]])
+
+new_sales_data.to_excel("feature_engineered_sales_listing.xlsx")
