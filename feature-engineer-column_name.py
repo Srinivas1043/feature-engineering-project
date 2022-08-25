8a07e24a273254bf63fd8bbb47ecd4a4dc6dab95
Srinivas <sm1043@gmail.com>
2022-08-25 18:25:16 +0200

    created function for featureengineer

--- feature-engineer-column_name.py
+++ feature-engineer-column_name.py
@@ -2,23 +2,42 @@ import pandas as pd
 import numpy as np
 
 
-# Read the excel file given
-purchase_sales_listing = pd.read_excel("Purchase_sales_listing.xlsx",sheet_name="Sales")
+def feature_engineer(filename, sheetname):
+    # Read the excel file given
+    print("We are reading the file....")
+    purchase_sales_listing = pd.read_excel("Purchase_sales_listing.xlsx",sheet_name="Sales")
 
-columnname_split = purchase_sales_listing.columns[0].split("-")
-values = purchase_sales_listing.values
+    #retrieve the splitted column name for the dataframe
+    columnname_split = purchase_sales_listing.columns[0].split("-")
+    
+    #retrieve the column values for the dataframe
+    values = purchase_sales_listing.values
 
-customernames = list()
-vechiclenumber =list()
+    #create a list to save customer names and vehicle numbers
+    customernames = list()
+    vechiclenumber =list()
 
-for i in values:
-    print(i[0].split("-"))
-    customernames.append(i[0].split("-")[0])
-    vechiclenumber.append(i[0].split("-")[1])
+    #loop through values to split the data and store it in the lists respectively
+    for i in values:
+        customernames.append(i[0].split("-")[0])
+        vechiclenumber.append(i[0].split("-")[1])
 
-purchase_sales_listing[columnname_split[0]] = customernames
-purchase_sales_listing[columnname_split[1]] = vechiclenumber
+    #copy values from list to the new columns
+    purchase_sales_listing[columnname_split[0]] = customernames
+    purchase_sales_listing[columnname_split[1]] = vechiclenumber
 
-new_sales_data = purchase_sales_listing.drop(columns=[purchase_sales_listing.columns[0]])
+    #drop the original column from the dataframe
+    new_sales_data = purchase_sales_listing.drop(columns=[purchase_sales_listing.columns[0]])
 
-new_sales_data.to_excel("feature_engineered_sales_listing.xlsx")
+
+    #export the new dataframe to an excel file
+    new_sales_data.to_excel("feature_engineered_sales_listing.xlsx")
+    print("Cheers!! Hip Hip HUrray! Your file is converted, kindly check the file and report if any errors!")
+
+
+
+print("Hey Raghava, hope you will be get the most out of our work!")
+
+user_filename = input("Enter file location and name with uppercase and lowercase carefully! Consider typing the file format as well")
+user_sheetname = input("Enter sheetname to be accessed kindly, with lowercase and uppercase letters.")
+feature_engineer(user_filename, user_sheetname)
